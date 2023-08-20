866cc7706fdfd652749b4835ef0817c2b18e6bcc
Amir Azimi <azymy3354@gmail.com>
2023-08-20 13:26:31 +0330

    add: header

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 const $ = document;
 const navBtn = $.querySelector(".nav__btn");
-const navMenu = $.querySelector(".nav-menu");
+const hamburger = $.querySelector(".hamburger-menu");
 navBtn.addEventListener("click", e => {
     navBtn.classList.toggle("nav__btn--open");
-    navMenu.classList.toggle("nav-menu--open");
+    hamburger.classList.toggle("hamburger-menu--open");
 });
