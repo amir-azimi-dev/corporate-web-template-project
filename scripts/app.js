266cbe371833bd42a36ecfb3b7da432c906b8f67
Amir Azimi <azymy3354@gmail.com>
2023-08-23 16:09:25 +0330

    add: svg-shapes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,19 @@
 const $ = document;
 const navBtn = $.querySelector(".nav__btn");
 const hamburger = $.querySelector(".hamburger-menu");
+const imgs = $.querySelectorAll("img");
+
 navBtn.addEventListener("click", e => {
     navBtn.classList.toggle("nav__btn--open");
     hamburger.classList.toggle("hamburger-menu--open");
+});
+
+imgs.forEach(img => {
+    img.setAttribute("loading", "lazy");
+});
+
+window.scrollTo({
+    top: window.scrollY,
+    left: 0,
+    behavior: "smooth"
 });
